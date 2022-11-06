35e072139266d5bffe06145035a964ff736108a4
eransam <eransam21@gmail.com>
2022-11-06 13:17:23 +0200

    first commit

--- src/components/Cate/Category.js
+++ src/components/Cate/Category.js
@@ -7,7 +7,7 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const Category = ({category, categories, addCategory ,delCategory}) => {
-
+    console.log("category.id: ", category.id.toString()+"1");
     let num =1
 function changeInputClass() {
     // const theTag = document.getElementsByClassName('NewCategoryDiv');
@@ -22,13 +22,14 @@ function openChildren(event) {
     // tar.innerText = "up";
     const viseOff = "viseOff";
     const vise = "vise";
+
     if (num%2===0) {
-        document.getElementById(toString(category.id)+"1").className = vise;
+        document.getElementById(category.id.toString()+"1").className = vise;
         num++
     }
     else
     {
-    document.getElementById(toString(category.id)+"1").className = viseOff;
+    document.getElementById(category.id.toString()+"1").className = viseOff;
     const tar=event.target;
     // tar.innerText = "down";
     num++
@@ -39,12 +40,13 @@ function openChildren(event) {
     return (
         <div id="mainNewCategory" >
         <ul className="category">
+            <button onClick={openChildren}>test</button>
             <li><div className="catName"><div>{" " +category.name}</div><DeleteCategory delCategory = {delCategory}  id = {category.id} /></div></li>
             <IconButton onClick={changeInputClass} aria-label="delete" size="large" >
             <AddCircleIcon fontSize="inherit" className="AddCircleIcon"/>
             </IconButton> 
             
-            <div id={toString(category.id)+"1"} className="vise">
+            <div id={category.id.toString()+"1"} className="vise">
             {
                 categories.map((childCategory) => category.id === childCategory.parentId && <Category  key={childCategory.id} addCategory={addCategory} delCategory={delCategory} category={childCategory} categories={categories} />)
             }
